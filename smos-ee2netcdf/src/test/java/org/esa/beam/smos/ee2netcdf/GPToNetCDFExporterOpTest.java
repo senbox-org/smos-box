3b250564e70e63b3acd21f650119ac1aa9edf470
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-10-28 09:59:01 +0100

    Deleted annoying tests

--- smos-ee2netcdf/src/test/java/org/esa/beam/smos/ee2netcdf/GPToNetCDFExporterOpTest.java
+++ smos-ee2netcdf/src/test/java/org/esa/beam/smos/ee2netcdf/GPToNetCDFExporterOpTest.java
@@ -19,10 +19,6 @@ public class GPToNetCDFExporterOpTest {
         assertEquals(1, declaredAnnotations.length);
         final OperatorMetadata operatorMetadata = (OperatorMetadata) declaredAnnotations[0];
         assertEquals("SmosGP2NetCDF", operatorMetadata.alias());
-        assertEquals("0.1", operatorMetadata.version());
-        assertEquals("Tom Block", operatorMetadata.authors());
-        assertEquals("(c) 2014 by Brockmann Consult", operatorMetadata.copyright());
-        assertEquals("Converts SMOS EE Products to NetCDF-GridPoint format.", operatorMetadata.description());
     }
 
     @Test
@@ -30,7 +26,6 @@ public class GPToNetCDFExporterOpTest {
         final Field institutionField = GPToNetCDFExporterOp.class.getDeclaredField("institution");
         final Parameter institutionFieldAnnotation = institutionField.getAnnotation(Parameter.class);
         assertEquals("", institutionFieldAnnotation.defaultValue());
-        assertEquals("Set institution field for file metadata. If left empty, no institution metadata is written to output file.", institutionFieldAnnotation.description());
     }
 
     @Test
@@ -38,15 +33,13 @@ public class GPToNetCDFExporterOpTest {
         final Field contactField = GPToNetCDFExporterOp.class.getDeclaredField("contact");
         final Parameter contactFieldAnnotation = contactField.getAnnotation(Parameter.class);
         assertEquals("", contactFieldAnnotation.defaultValue());
-        assertEquals("Set contact field for file metadata. If left empty, no contact information is written to output file.", contactFieldAnnotation.description());
     }
 
     @Test
     public void testParameterAnnotations_outputBandNames() throws NoSuchFieldException {
         final Field bandNamesField = GPToNetCDFExporterOp.class.getDeclaredField("outputBandNames");
-        final Parameter bandnamesFieldAnnotation = bandNamesField.getAnnotation(Parameter.class);
-        assertEquals("", bandnamesFieldAnnotation.defaultValue());
-        assertEquals("Comma separated list of band names to export. If left empty, no band subsetting is applied.", bandnamesFieldAnnotation.description());
+        final Parameter bandNamesFieldAnnotation = bandNamesField.getAnnotation(Parameter.class);
+        assertEquals("", bandNamesFieldAnnotation.defaultValue());
     }
 
     @Test
@@ -56,6 +49,5 @@ public class GPToNetCDFExporterOpTest {
         assertEquals("6", compressionLevelFieldAnnotation.defaultValue());
         final String[] valueSet = compressionLevelFieldAnnotation.valueSet();
         assertArrayEquals(new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}, valueSet);
-        assertEquals("Output file compression level. 0 - no compression, 9 - highest compression.", compressionLevelFieldAnnotation.description());
     }
 }
