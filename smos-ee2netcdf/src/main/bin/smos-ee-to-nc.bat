4ec5ff55c1667def0d61eaa31b9b61c9007ac899
tom <tom.block@brockmann-consult.de>
2016-01-20 11:38:23 +0100

    increased java heap

--- smos-ee2netcdf/src/main/bin/smos-ee-to-nc.bat
+++ smos-ee2netcdf/src/main/bin/smos-ee-to-nc.bat
@@ -12,7 +12,7 @@ SET EE_TO_NETCDF_DIR=%~dp0%
 ::    '-Xms64M' sets the minimum heap space to 64 megabytes
 ::    '-Xmx512M' sets the maximum heap space to 512 megabytes
 ::------------------------------------------------------------------
-SET JAVA_OPTS=-Xms64M -Xmx1024M
+SET JAVA_OPTS=-Xms64M -Xmx1224M
 
 
 :: -======================-
